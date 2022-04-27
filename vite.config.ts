6dea72af22bb0bc4e50ccb5262d4c8bc94f83fb5
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2022-04-27 12:12:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/wai/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   build: {
     sourcemap: true
   }
