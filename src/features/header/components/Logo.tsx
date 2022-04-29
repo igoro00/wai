e248de9bc2c577c1398c02f19e2efc7f531414e7
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2022-04-29 22:29:07 +0200

    Logo click (#1)
    * Update Logo.tsx
    
    * Update App.tsx
    
    * Update index.tsx
    
    * cursor pointer navbar buttons

--- src/features/header/components/Logo.tsx
+++ src/features/header/components/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 interface Props {
-    
+    onClick?: ()=>void
 }
 
-const Logo:React.FC<Props> = ({}) => {
+const Logo:React.FC<Props> = ({onClick=()=>{}}) => {
     return (
-        <h1 style={{margin:0}}>
+        <h1 style={{margin:0, cursor:"pointer"}} onClick={onClick}>
             LOGO
         </h1>
     )
